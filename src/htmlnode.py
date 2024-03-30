753e385fb17c093035bafdd1ba28a5819470818f
nwpulverizer <nathan.w.pulver@gmail.com>
2024-03-29 22:08:48 -0700

    added parent html class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,6 +9,8 @@ class HTMLNode:
         raise NotImplementedError
 
     def props_to_html(self):
+        if self.props is None:
+            return ""
         htmlstr = ""
         for k, v in self.props.items():
             htmlstr += f' {k}="{v}"'
@@ -19,13 +21,28 @@ class HTMLNode:
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, value, tag=None, props=None):
+    def __init__(self, value, tag, props=None):
         super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self):
         if self.value is None:
             raise ValueError
-        if self.tag == None:
+        if self.tag is None:
             return self.value
         else:
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, children, tag=None, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent requires a tag")
+        if self.children is None:
+            raise ValueError("Parent requires children")
+        child_string = ""
+        for child in self.children:
+            child_string += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{child_string}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class HTMLNodeTest(unittest.TestCase):
@@ -34,6 +34,70 @@ class LeafNodeTest(unittest.TestCase):
     def test_tohtml(self):
         leaf = LeafNode("value", tag="a", props={"href": "https://url.com"})
         self.assertEqual('<a href="https://url.com">value</a>', leaf.to_html())
+
+
+class ParentNodeTest(unittest.TestCase):
+    def test_tohtml_noprops(self):
+        node = ParentNode(
+            tag="p",
+            children=[
+                LeafNode("Bold Text", "b"),
+                LeafNode("Plain ol' text", None),
+                LeafNode("italic/spicy txt", "i"),
+            ],
+        )
+        self.assertEqual(
+            "<p><b>Bold Text</b>Plain ol' text<i>italic/spicy txt</i></p>",
+            node.to_html(),
+        )
+
+    def test_tohtml_props(self):
+        node = ParentNode(
+            tag="p",
+            children=[
+                LeafNode("Bold Text", "b"),
+                LeafNode("Plain old text", None),
+                LeafNode("italic/spicy txt", "i"),
+            ],
+            props={"style": "color:blue"},
+        )
+        self.assertEqual(
+            '<p style="color:blue"><b>Bold Text</b>Plain old text<i>italic/spicy txt</i></p>',
+            node.to_html(),
+        )
+
+    def test_tohtml_single_nested_parents(self):
+        node = ParentNode(
+            tag="p",
+            children=[
+                LeafNode("Bold Text", "b"),
+                LeafNode("Plain old text", None),
+                LeafNode("italic/spicy txt", "i"),
+            ],
+            props={"style": "color:blue"},
+        )
+        node2 = ParentNode(tag="div", children=[node])
+        self.assertEqual(
+            '<div><p style="color:blue"><b>Bold Text</b>Plain old text<i>italic/spicy txt</i></p></div>',
+            node2.to_html(),
+        )
+
+    def test_tohtml_double_nested_parents(self):
+        node = ParentNode(
+            tag="p",
+            children=[
+                LeafNode("Bold Text", "b"),
+                LeafNode("Plain old text", None),
+                LeafNode("italic/spicy txt", "i"),
+            ],
+            props={"style": "color:blue"},
+        )
+        node2 = ParentNode(tag="div", children=[node])
+        node3 = ParentNode(tag="div", children=[node2])
+        self.assertEqual(
+            '<div><div><p style="color:blue"><b>Bold Text</b>Plain old text<i>italic/spicy txt</i></p></div></div>',
+            node3.to_html(),
+        )
 
 
 if __name__ == "__main__":
