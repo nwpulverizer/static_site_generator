09186827aac606d12f87726f20deed84b5c93c87
nwpulverizer <nathan.w.pulver@gmail.com>
2024-03-30 21:30:17 -0700

    changed tests to have TextType enum instead of strings

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,31 +1,31 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, TextType
 
 
 class TextNodeTest(unittest.TestCase):
     def test_eq(self):
-        node1 = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node1 = TextNode("This is a text node", TextType.bold)
+        node2 = TextNode("This is a text node", TextType.bold)
         self.assertEqual(node1, node2)
 
     def test_ne(self):
-        node1 = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold1")
+        node1 = TextNode("This is a text node but different", TextType.text)
+        node2 = TextNode("This is a text node", TextType.text)
         self.assertNotEqual(node1, node2)
 
     def test_emptystrurl(self):
-        node1 = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold", "")
+        node1 = TextNode("This is a text node", TextType.bold)
+        node2 = TextNode("This is a text node", TextType.bold, "")
         self.assertNotEqual(node1, node2)
 
     def test_fullurl_eq(self):
-        node1 = TextNode("This is a text node", "bold", "http://url.com")
-        node2 = TextNode("This is a text node", "bold", "http://url.com")
+        node1 = TextNode("This is a text node", TextType.bold, "http://url.com")
+        node2 = TextNode("This is a text node", TextType.bold, "http://url.com")
         self.assertEqual(node1, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", "bold", "http://url.com")
+        node = TextNode("This is a text node", TextType.bold, "http://url.com")
         self.assertEqual(
             "TextNode(This is a text node, bold, http://url.com)", repr(node)
         )
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from enum import Enum
 
 
@@ -30,7 +30,40 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    # take in a line and find any nested textnode types of the
-    # specified type based on the delimiter
-    pass
+def split_nodes_delimiter(
+    old_nodes: List[TextNode], delimiter: str, text_type: TextType
+):
+    # markdown specifies the deliminter and text type - as in
+    # if the delimiter is `, then it is code text type. Why do we need
+    # to have text_type also?
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type.name != "text":
+            new_nodes.append(node)
+        elif text_type.name != "link" or text_type.name != "image":
+            # this will probably not work for image types right?
+            # image is ![alt text](image.url)
+            # I could split on ![ so that I make sure
+            # I am not just hitting a exclamation point.
+            split_text = node.text.split(delimiter)
+            # the even indexes will always be the text nodes.
+            updated_nodes = []
+            for i, splited in enumerate(split_text):
+                if i % 2 == 0:
+                    updated_nodes.append(TextNode(splited, TextType.text))
+                else:
+                    updated_nodes.append(TextNode(splited, text_type))
+            new_nodes.extend(updated_nodes)
+        # elif text_type.name == "link":
+        # do the things for link markdown
+
+        # elif text_type.name == "image":
+        # do the thing for image markdown
+        # in this case my delimitter will be ![
+        # example: text with image ![alt](image.jpg)
+        # split on ![: text with image, alt](image.jpg)
+        #  first part will be text
+        #  second part need to split ] to get alt,
+        #  then on ) and lstrip ( to get image url
+
+        return new_nodes
